adc90a6c32bb3c7c528a6e201ceac14997aed7b0
arigato_dl <dulguun.mongolia@yahoo.com>
2015-10-13 00:02:25 +0900

    + Changed to mysql + Added mysql connector class

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,59 +1,19 @@
 __author__ = 'Dulguun'
 
-import schedule
-import time
-import logging
-from pymongo import MongoClient
+import ConfigParser
 
-from Parser.Parser import myParser
-from Database.DBManager import write_to_db
-from Database.DBManager import retrieve_documents
-from Server.Server import push_to_server
-from Server.Server import receive_and_check
+from Database.DBManager import DBManager
 
-#CONSTANTS
-NUM_OF_DOCUMENTS = 10
-SOURCES = {"GoGo Мэдээ", "iKon.mn - Шинэ мэдээ"}
+CONFIG_FILE_NAME = ".env"
 
-logging.basicConfig(filename=time.strftime("%Y%m%d")+".log",level=logging.INFO)
-db = MongoClient().feedAr
+config = ConfigParser.ConfigParser()
+config.read(CONFIG_FILE_NAME)
 
-def get_and_push():
-    _parser = myParser()
-    _parser.parse()
+_db_host = config.get("Database", "DB_HOST")
+_db_database = config.get("Database", "DB_DATABASE")
+_db_username = config.get("Database", "DB_USERNAME")
+_db_password = config.get("Database", "DB_PASSWORD")
 
-    added_contents = 0
-    for feed_entries in _parser.feeds:
-        added_contents += write_to_db(feed_entries.entries, feed_entries.feed.title, db)
+db_manager = DBManager(_db_host, _db_username, _db_password, _db_database)
 
-    logging.info(time.strftime("%H:%M:%S") + " added " + str(added_contents) + " content(s) to database")
-
-    result_by_sources = []
-
-    for source in SOURCES:
-        result_by_sources.append(retrieve_documents(NUM_OF_DOCUMENTS, source, db))
-
-    feeds_to_write = receive_and_check(result_by_sources)
-    print(feeds_to_write)
-
-    server_response = push_to_server(feeds_to_write)
-    logging.info(server_response)
-
-#TODO currently not working
-def log_file_changer():
-    file_name = time.strftime("%Y%m%d")+".log"
-    file_handler = logging.FileHandler(file_name, 'a')
-
-    log = logging.getLogger()  # root logger
-    for handler in log.handlers:  # remove all old handlers
-        log.removeHandler(handler)
-    log.addHandler(file_handler)      # set the new handler
-
-get_and_push()
-log_file_changer()
-schedule.every(1).minutes.do(get_and_push)
-#schedule.every().day.at("00:00").do(log_file_changer)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+print db_manager.select()
